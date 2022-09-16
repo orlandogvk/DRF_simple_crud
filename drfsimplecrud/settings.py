711da1c61c458e6435b390a70d45431036468adb
Orlando Rodriguez <orlandog79@gmail.com>
2022-09-15 20:30:11 -0500

    Cambios en el proyecto

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
+    'rest_framework_swagger',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
 
-# Create your views here.
+
+schema_view = get_swagger_view(title='Project API')
+
+urlpatterns = [
+    url(r'^$', schema_view)
+]
+
