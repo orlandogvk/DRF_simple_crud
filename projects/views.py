d7d5636e0f6db420199ae3c8d7bcba54a57672e8
Orlando Rodriguez <orlandog79@gmail.com>
2022-09-15 20:47:46 -0500

    Delete content views.py

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
-
-
-schema_view = get_swagger_view(title='Project API')
-
-urlpatterns = [
-    url(r'^$', schema_view)
-]
 
